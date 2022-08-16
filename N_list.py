fa170a0ec8fabc2313e99a718e917db3341af42a
jyotirmayeemohanta <97024151+jyotirmayeemohanta@users.noreply.github.com>
2022-08-16 19:29:18 +0530

    Add files via upload

---
+++ N_list.py
@@ -0,0 +1,19 @@
+a = 123
+k = str(a)
+b = []
+ 
+for i in range ( 0 , len(k) ):
+        b.append(int(k[i]))         
+print(b)
+
+
+#Convert a list to a string
+ 
+# a = ['a','b','c','d','e']
+ 
+# strs = (''.join(a)) 
+# strs =''
+ 
+# for i in a:
+#     strs = strs + i
+# print(strs)
