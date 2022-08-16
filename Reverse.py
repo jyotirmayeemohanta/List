024e176deb87b7bb2b33680aac3703f67ce9226b
jyotirmayeemohanta <97024151+jyotirmayeemohanta@users.noreply.github.com>
2022-08-16 14:45:20 +0530

    Add files via upload

---
+++ Reverse.py
@@ -0,0 +1,17 @@
+# a = [1, 3, 4, 5, 8, 10, 19] 
+# b = a[::-1] 
+# print(b)
+
+
+# a = ['jyoti','alok','nitu','lisa']
+# b = [] 
+# for i in range(len(a) - 1 , -1, -1):
+#     b.append(a[i])     
+# print(b)
+
+
+a = [1, 3, 4, 5, 8, 10, 19]
+b = []
+for i in range(len(a) - 1 , -1, -1):
+    b.append(a[i])     
+print(b)
